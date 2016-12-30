1077303b5e22eaff20fedb75eb6216b16cf46e73
Simos Gerasimou <simos@cs.york.ac.uk>
2016-12-30 10:48:34 +0200

    Added moos-ivp-extend

---
+++ moos-ivp-extend/src/sOdometry/Odometry.h
@@ -0,0 +1,33 @@
+/************************************************************/
+/*    NAME: Simos Gerasimou                                              */
+/*    ORGN: MIT                                             */
+/*    FILE: Odometry.h                                          */
+/*    DATE:                                                 */
+/************************************************************/
+
+#ifndef Odometry_HEADER
+#define Odometry_HEADER
+
+#include "MOOS/libMOOS/MOOSLib.h"
+
+class Odometry : public CMOOSApp
+{
+ public:
+   Odometry();
+   ~Odometry();
+
+ protected:
+   bool OnNewMail(MOOSMSG_LIST &NewMail);
+   bool Iterate();
+   bool OnConnectToServer();
+   bool OnStartUp();
+   void RegisterVariables();
+
+ private: // Configuration variables
+
+ private: // State variables
+   unsigned int m_iterations;
+   double       m_timewarp;
+};
+
+#endif 
